c3a45f2526e77b1d2b478c376002c9bd01445706
Jon Scheaffer <84929443+J-Schea29@users.noreply.github.com>
2021-10-13 13:16:49 -0700

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,67 +1,69 @@
-function markerSize(population) {
-    return Math.sqrt(population) * 500;
+function markerSize(value) {
+    return value * 20000;
   }
 
 function convertTimestamp(timestamp) {
-
-        var time = new Date(timestamp)
-        return timeConverted = time.getDate()+
-            "/"+(time.getMonth()+1)+
-            "/"+time.getFullYear()+
-            " "+time.getHours()+
-            ":"+time.getMinutes()+
-            ":"+time.getSeconds()
+  var time = new Date(timestamp)
+  return timeConverted = time.getDate()+
+      "/"+(time.getMonth()+1)+
+      "/"+time.getFullYear()+
+      " "+time.getHours()+
+      ":"+time.getMinutes()+
+      ":"+time.getSeconds()
 }
 
-var myMap = L.map("mapid", {
-    center: [36.778259, -119.417931],
-    zoom: 3
-  });
+function colorElevate(location) {
   
-  // Add a tile layer.
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
+  if (location.coordinates[2] <= 10) {
+    color = "#4cbb17"
+  } else if (location.coordinates[2] <= 30 && location.coordinates[2] > 10) {
+    color = "#ADFF2F"
+  } else if (location.coordinates[2] <= 50 && location.coordinates[2] > 30) {
+    color = "#FFFF00"
+  } else if (location.coordinates[2] <= 70 && location.coordinates[2] > 50) {
+    color = "#FFD700"
+  } else if (location.coordinates[2] <= 90 && location.coordinates[2] > 70) {
+    color = "#FFA500"
+  } else {
+    color = "#FF6347"
+  }
+  return color;
+}
+
+// Creating the map object
+var myMap = L.map("map", {
+  center: [40.71, -116.10],
+  zoom: 5
+});
+
+// Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
 
 
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
 d3.json(url, function(response) {
 
-  console.log(response)
   var features = response.features
-  
-  console.log(features)
 
-  var QuakeArray = []
-  var DepthArray = []
-  var MagArray = []
-  var date = []
   for (var i = 0; i < features.length; i++) {
     var location = features[i].geometry;
     var properties = features[i].properties;
+    var timestamp = properties.time
+    var timeConverted = convertTimestamp(timestamp)
+    colorElevate(location);
 
-    if (location) {
-        QuakeArray.push([location.coordinates[1], location.coordinates[0]]);
-        DepthArray.push(location.coordinates[2])
-    }
-
-    if (properties) {
-        MagArray.push(properties.mag);
-        var timestamp = properties.time
-        var timeConverted = convertTimestamp(timestamp)
-        date.push(timeConverted);
-    }
-    // var color = "";
     L.circle([location.coordinates[1], location.coordinates[0]], {
-        fillColor: location.coordinates[2],
-        colorscale:"Earth",
+        color: "black",  
+        fillColor: color,
+        weight: .5,
+        fillOpacity: .5,
         // Adjust the radius.
         radius: markerSize(properties.mag)
-      }).bindPopup(`<h1>${properties.place}</h1> <hr> <h3>Time: ${timeConverted}</h3>`).addTo(myMap);
+      }).bindPopup(`<h1>${properties.place}</h1> <hr> <h3>Time: ${timeConverted}</h3> <h3>Magnitude: ${properties.mag}<h3> <h3>Elevation: ${location.coordinates[2]}<h3>`).addTo(myMap);
   }
-  console.log(MagArray)
-  console.log(date)
 });
 
 
